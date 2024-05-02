3571e851ddc827581ec7bb6f9d335e59421ab80f
Srishti003 <90563430+Srishti003@users.noreply.github.com>
2024-05-02 18:45:47 +0000

    01_basics done

---
+++ 01_basics/06_math.js
@@ -0,0 +1,12 @@
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10)+1))
+
+const min_start = 10
+const max_end = 20
+
+console.log(Math.floor((Math.random()*(max_end - min_start+1))+1) +min_start)
+
+
+
+// node 01_basics/06_math
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,7 @@
+// array 
+const myArr = [0, 1, 2, 3, 4, 5, true , "Srishti", NaN, undefined]
+console.log(myArr)
+
+
+
+//node 02_basics/01_arrays.js
