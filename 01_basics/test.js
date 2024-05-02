a84603680a07f4b631f275380e3999adacaf7662
Srishti003 <90563430+Srishti003@users.noreply.github.com>
2024-05-02 04:16:56 +0000

    1

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("hey")
+//node 01_basics/test.js
+const accountId = 1;
+let account_name = "Srishti Chaturvedi"
+var account_balance = 200000000;
+account_place= "Jaipur" //haan aise bhi likh skte hai , but not advised.
+let account_nominee
+console.table([accountId, account_balance, account_name,account_place ,account_nominee])
+
+account_name= "Chaturvedi"
+account_balance="4000000000000000"
+console.table([accountId, account_balance, account_name,account_place ,account_nominee])
+
