f6554bed570114613fcb4023c66ab25a956fc4f2
Srishti003 <90563430+Srishti003@users.noreply.github.com>
2024-05-02 06:01:05 +0000

    2

---
+++ 01_basics/01_letConstVar.js
@@ -0,0 +1,15 @@
+//node 01_basics/test.js
+const accountId = 1;
+let account_name = "Srishti Chaturvedi"
+var account_balance = 200000000;
+account_place= "Jaipur" //haan aise bhi likh skte hai , but not advised.
+let account_nominee
+console.table([accountId, account_balance, account_name,account_place ,account_nominee])
+
+account_name= "Chaturvedi"
+account_balance="4000000000000000"
+console.table([accountId, account_balance, account_name,account_place ,account_nominee])
+
+//var is not prefered to be used because it has issues in block scope and functional scope
+
+
--- 01_basics/02.js
+++ 01_basics/02.js
@@ -1,1 +1,8 @@
-console.log("2")
+//number -> range -> 2t to the power 53
+//bigInt 
+//boolean -> true/false
+//string -> "Srishti"
+//null-> matlab usme hum bta rhe hai ki isme khali value stored hai--> object 
+//undefined -> matlab ab tak value he assign nahi hui hai-->undefined
+console.log( typeof null )   //object 
+console.log(typeof(undefined))  // undefined
