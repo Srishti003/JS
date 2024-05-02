191f5a834e5db8545aaabae9a720e72eff2a69f5
Srishti003 <90563430+Srishti003@users.noreply.github.com>
2024-05-02 08:03:46 +0000

    3

---
+++ 01_basics/03.js
@@ -0,0 +1,25 @@
+let val = 33;//number 
+console.log(typeof(String(val)))
+let val1 = 33;//number 
+console.log(typeof(String(val)))
+ let val2 = "33abc"
+ console.log(typeof(Number(val2)))//jabki yahan pe number print nahi hona chahiye tha 
+
+ console.log(typeof(Number(NaN)) )
+ console.log(typeof(Number(undefined)) )
+
+ console.log(typeof(Number(true)))
+ console.log(typeof(Number(false)) )
+
+ console.log(typeof(Number("Srishti")) )
+ console.log(Number("Srishti"))
+ 
+ console.log(typeof(Number("")) )
+ console.log(Number(""))
+
+ console.log(typeof(Boolean("Srishti")) )
+ console.log(Boolean("Srishti"))
+ 
+ 
+
+//  node 01_basics/03.js
